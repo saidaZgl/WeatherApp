dc60e02c4461c11cf38650befb25be1f66e144c4
Saida Zghal <zghal.saida@orange.fr>
2020-03-07 20:57:48 +0100

    modif connection

--- models/connection.js
+++ models/connection.js
@@ -6,12 +6,8 @@ var options = {
   useNewUrlParser: true
 };
 
-mongoose.connect(
-  "mongodb+srv://capsule:k3RqNc5rHqXPh3wJ@saidazgl-0zpfc.mongodb.net/weatherapp?retryWrites=true&w=majority",
-  options,
-  function(err) {
-    console.log(err);
-  }
-);
+mongoose.connect("mongodb+srv://...", options, function(err) {
+  console.log(err);
+});
 
 module.exports = mongoose;
